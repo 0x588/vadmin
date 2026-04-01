4a97f2759b7720531b080e5cd0da26865884e639
0x588 <pengjay.cn@gmail.com>
2026-04-01 08:39:03 +0800

    fix: fixed bugs

--- admin/src/views/product/spec/data.ts
+++ admin/src/views/product/spec/data.ts
@@ -20,7 +20,7 @@ export function useFormSchema(): VbenFormSchema[] {
     {
       component: 'Select',
       componentProps: {
-        options: getDictOptions(DICT_TYPE.SPEC_TYPE, 'string'),
+        options: getDictOptions(DICT_TYPE.SPEC_TYPE),
       },
       fieldName: 'type',
       label: $t('product.spec.type'),
